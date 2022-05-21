947538d5f4050c7c81a0c36b3a85397d0173c5fc
caioreis123 <reis.caio@gmail.com>
2022-05-21 08:29:36 -0300

    refatoracao para deixar mais compreensivel

--- src/caio/luiz/questao1/Questao1.java
+++ src/caio/luiz/questao1/Questao1.java
@@ -1,13 +1,12 @@
 package caio.luiz.questao1;
 
 import caio.luiz.Equipamento;
-import caio.luiz.Halteres;
 
 import java.io.FileInputStream;
+import java.lang.reflect.Constructor;
 import java.util.Properties;
 
 public class Questao1 {
-//    tipoDeEquipamento será a chave e identificador um valor, lidos de um arquivo de configuracao properties
     public static Equipamento criarEquipamento(String tipoDeEquipamento, String identificador, int quantidade){
 //        equipamentos de mesmo tipo podem ter identificadores diferentes
 //        quando o identificador solicitado for igual a um existente, deve-se apenas atualizar sua quantidade, em vez de criar uma nova instancia
@@ -17,15 +16,14 @@ public class Questao1 {
         try {
             appProps.load(new FileInputStream(appConfigPath));
             String className = appProps.getProperty(tipoDeEquipamento);
-            Class<?> c = Class.forName("caio.luiz."+className);
-            Object equipamento = c.getDeclaredConstructor(String.class, int.class).newInstance(identificador, quantidade);
-            System.out.println(c);
+            Class<?> classe = Class.forName("caio.luiz."+className);
+            Constructor<?> construtor = classe.getDeclaredConstructor(String.class, int.class);
+            Object equipamento = construtor.newInstance(identificador, quantidade);
+            System.out.println(classe);
             return (Equipamento) equipamento;
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
         return null;
     }
 }
